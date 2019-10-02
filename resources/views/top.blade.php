5f7a605b7d9f24331022f5bcd171d58d26d9d032
tomoya <t0819t@ezweb.ne.jp>
2019-10-02 12:36:57 +0000

    ディベロップターム

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,23 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+   <h1>miniseum</h1>
+   <h2>-最小限の博物館-</h2>
+   <p>ミニマリスト・シンプリストの残したアイテム達</p>
+   
+   
+   <head>
+       <link href="https://fonts.googleapis.com/css?family=Spirax|Tulpen+One&display=swap" rel="stylesheet">
+       <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP&display=swap" rel="stylesheet">
+   </head>
+   
+   <style>
+   
+   h1 {
+       font-size: 70px;
+       font-weight: lighter;
+       font-family: 'Spirax', cursive;
+       text-align: center;
+        margin-top: 100px;
+        height: 30px;
+   }
+   h2 {
+           font-size: 18px;
+           font-family: Noto Sans JP;
+           text-align: center;
+           margin-top: 0px;
+   }
+   p {
+       text-align: center;
+       font-family: Noto Sans JP;
+   }
+   </style>
+  
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
