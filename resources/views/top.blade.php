0639b99df007dd12c1a014ade8f6b601d2f52d0e
tomoya <t0819t@ezweb.ne.jp>
2019-10-04 07:57:44 +0000

    ディベロップターム

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,38 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-   <h1>miniseum</h1>
-   <h2>-最小限の博物館-</h2>
-   <p>ミニマリスト・シンプリストの残したアイテム達</p>
-   
-   
-   <head>
-       <link href="https://fonts.googleapis.com/css?family=Spirax|Tulpen+One&display=swap" rel="stylesheet">
-       <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP&display=swap" rel="stylesheet">
-   </head>
-   
-   <style>
-   
-   h1 {
-       font-size: 70px;
-       font-weight: lighter;
-       font-family: 'Spirax', cursive;
-       text-align: center;
-        margin-top: 100px;
-        height: 30px;
-   }
-   h2 {
-           font-size: 18px;
-           font-family: Noto Sans JP;
-           text-align: center;
-           margin-top: 0px;
-   }
-   p {
-       text-align: center;
-       font-family: Noto Sans JP;
-   }
-   </style>
+    <div class="container">    
+       <h1>miniseum</h1><br><br><br><br><br>
+       <h2>-最小限の博物館-</h2>
+       <p>ミニマリスト・シンプリストの残したアイテム達</p>
+    </div>
+@endsection
+
+@section('style')
+ <style>
+     body{
+       background-image: url("/images/top.jpg");
+       margin-top: 200px;
+       
+      
   
-
-
+     }
+     
+ </style>
 @endsection
